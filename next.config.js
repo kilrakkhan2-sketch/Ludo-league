3a8ac0944a3eda75b43d012885dc2d6cb69c24e0
Kilrak Khan <kilrakkhan2@gmail.com>
2025-12-22 03:05:41 +0000

    1 of 1 error Next.js (14.2.35) is outdated (learn more)
    Unhandled Runti

--- next.config.js
+++ next.config.js
@@ -20,43 +20,4 @@ const nextConfig = {
   },
 };
 
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      // Match all API routes
-      urlPattern: /^\/api\/.*/,
-      handler: 'NetworkOnly',
-    },
-    {
-      // Match all navigation requests (pages)
-      urlPattern: ({ request }) => request.mode === 'navigate',
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'pages',
-        expiration: {
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-        },
-      },
-    },
-    {
-      // Match other requests (images, css, js, etc.)
-      urlPattern: /.*/,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'assets',
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-        },
-      },
-    }
-  ],
-});
-
-
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
