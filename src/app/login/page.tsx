8646408515dc9cae720e87ac6dee6520a0eaa4f3
Kilrak Khan <kilrakkhan2@gmail.com>
2025-12-23 05:51:13 +0000

    1 of 1 error Next.js (14.2.35) is outdated (learn more)
    Unhandled Runti

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import { useState } from 'react';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useAuth } from '@/firebase';
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import { useState } from 'react';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useAuth, useFirestore } from '@/firebase';
