1ed0b6b9ccc8f5814671f973c3751b5e206aeaad
Kilrak Khan <kilrakkhan2@gmail.com>
2025-12-26 04:30:41 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -19,6 +19,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
       }
     ],
   },
