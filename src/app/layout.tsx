771ffbd568c712240cc24deec96694249c529e31
Kilrak Khan <kilrakkhan2@gmail.com>
2026-01-07 03:36:56 +0000

    GET https://6000-firebase-studio-1767412894590.cluster-edb2jv34dnhjisxuq

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
         <link rel="icon" type="image/png" href="/icon-192x192.png" />
+        <link rel="shortcut icon" href="/icon-192x192.png" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
